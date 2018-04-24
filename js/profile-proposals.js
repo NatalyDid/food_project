b1c32c48fe3553da947dedd40657bf1290e3f54d
Unknown <nat.khom@ukr.net>
2018-04-24 17:15:57 +0300

    files added

--- js/profile-proposals.js
+++ js/profile-proposals.js
@@ -2,12 +2,12 @@ $(function () {
     $('#offer').on('click', function (ev) {
         ev.preventDefault();
         var id = ev.target.dataset.id;
-        if(id){
-            var modal =  $('#delete-confirm-modal-id');
+        if (id) {
+            var modal = $('#delete-confirm-modal-id');
             modal.modal('show');
             modal.on('shown.bs.modal', function () {
                 $('#delete-confirm-button-modal-id').on('click', function (ev) {
-                   console.log(id)
+                    console.log(id);
                     modal.modal('hide');
                 });
             });
