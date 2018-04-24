6f3a43165a37f240cbfe257b92db5f67ad627b5d
Unknown <nat.khom@ukr.net>
2018-04-24 16:46:04 +0300

    files updated

---
+++ js/profile-proposals.js
@@ -0,0 +1,16 @@
+$(function () {
+    $('#offer').on('click', function (ev) {
+        ev.preventDefault();
+        var id = ev.target.dataset.id;
+        if(id){
+            var modal =  $('#delete-confirm-modal-id');
+            modal.modal('show');
+            modal.on('shown.bs.modal', function () {
+                $('#delete-confirm-button-modal-id').on('click', function (ev) {
+                   console.log(id)
+                    modal.modal('hide');
+                });
+            });
+        }
+    });
+});
