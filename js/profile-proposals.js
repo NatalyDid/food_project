baf61f58058d052b2ce07c850fcaf1449ad055f5
Unknown <nat.khom@ukr.net>
2018-04-26 15:40:03 +0300

    files updated

--- js/profile-proposals.js
+++ js/profile-proposals.js
@@ -3,17 +3,24 @@ $(function () {
         $('.limit').toggle();
     });
 
-    $('#offer').on('click', function (ev) {
-        var id = ev.target.dataset.id;
-        if (id) {
-            var modal = $('#delete-confirm-modal-id');
-            modal.modal('show');
-            modal.on('shown.bs.modal', function () {
-                $('#delete-confirm-button-modal-id').on('click', function (ev) {
-                    console.log(id);
-                    modal.modal('hide');
+    function modalButtonsEvents(blockId, modalId, buttonId){
+        $(blockId).on('click', function (ev) {
+            var id = ev.target.dataset.id;
+            if (id) {
+                var modal = $(modalId);
+                modal.modal('show');
+                modal.on('shown.bs.modal', function () {
+                    $(buttonId).on('click', function (ev) {
+                        console.log(id);
+                        modal.modal('hide');
+                    });
                 });
-            });
-        }
-    });
+            }
+        });
+    }
+
+    modalButtonsEvents('#offer', '#delete-confirm-modal-id', '#delete-confirm-button-modal-id');
+
+    modalButtonsEvents('#proposals', '#modal-pre-order', '#confirm-button-modal-id');
+
 });
